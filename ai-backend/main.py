f824d67812c4506d2bf82de93874a84552ec7e20
Anuj Mumbaikar <anujmumbaikar120405@gmail.com>
2026-03-26 02:58:19 +0530

    Commit9: Setup endpoints for file upload and chat with LLM.

--- ai-backend/main.py
+++ ai-backend/main.py
@@ -1,18 +1,26 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-from langchain_qdrant import QdrantVectorStore
+from ingestion import ingest_file_to_vector_db
 from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
 from openai import OpenAI
 from dotenv import load_dotenv
+from fastapi import FastAPI , File, UploadFile, BackgroundTasks
+from pydantic import BaseModel
+import os
+import uuid
 
 load_dotenv()
-
-app = FastAPI()
 client = OpenAI()
+app = FastAPI()
 
 embedding_model = OpenAIEmbeddings(
     model="text-embedding-3-large"
 )
+
+class ChatRequest(BaseModel):
+    query: str
+
+UPLOAD_DIR = "uploaded_files"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 vector_db = QdrantVectorStore.from_existing_collection(
     embedding=embedding_model,
@@ -20,33 +28,69 @@ vector_db = QdrantVectorStore.from_existing_collection(
     collection_name="multimodal_collection",
 )
 
-# request body
-class QueryRequest(BaseModel):
-    query: str
+@app.post("/upload")
+async def upload_file(
+    background_tasks: BackgroundTasks,
+    file: UploadFile = File(...)
+):
+    file_id = str(uuid.uuid4())
+    file_path = f"{UPLOAD_DIR}/{file_id}_{file.filename}"
 
-# endpoint
-@app.post("/query")
-def query_docs(req: QueryRequest):
+    with open(file_path, "wb") as f:
+        f.write(await file.read())
 
-    results = vector_db.similarity_search(req.query, k=5)
+    background_tasks.add_task(ingest_file_to_vector_db, file_path)
 
-    context = "\n\n".join([
-        r.page_content for r in results
+    return {
+        "message": "File uploaded, processing started",
+        "file_id": file_id
+    }
+
+
+@app.post("/chat")
+async def chat_endpoint(request: ChatRequest):
+
+    query = request.query
+
+    search_results = vector_db.similarity_search(query, k=6)
+
+    context = "\n\n\n".join([
+        f"""
+            Page Content:
+            {res.page_content}
+
+            Metadata:
+            Type: {res.metadata.get("type")}
+            Filename: {res.metadata.get("filename")}
+            Caption: {res.metadata.get("caption")}
+            Page Number: {res.metadata.get("page_number")}
+            """
+                    for res in search_results
     ])
 
     SYSTEM_PROMPT = f"""
-    Answer ONLY using the context below.
-    If not found, say: Answer not found.
+        You are a context-aware AI assistant for document understanding.
 
-    Context:
-    {context}
+            The context may include text, tables, and image descriptions.
+
+            Guidelines:
+            - Use only the provided context to answer
+            - If the context includes tables, summarize key insights
+            - If the context includes image descriptions, explain them clearly
+            - Do not hallucinate or add external knowledge
+            - If answer is missing, say: "Answer not found in the document"
+
+            Answer clearly and in a structured way.
+
+            Context:
+            {context}
     """
 
     response = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
             {"role": "system", "content": SYSTEM_PROMPT},
-            {"role": "user", "content": req.query}
+            {"role": "user", "content": query}
         ],
     )
 
